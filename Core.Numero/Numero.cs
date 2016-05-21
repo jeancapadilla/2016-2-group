647a4e691009c4dea86f87d57553292be86b4c47
JeanCa <padilla-jean@hotmail.com>
2016-05-21 16:29:14 -0600

    version inicial de la calculadora incluye la clase core para el numero

---
+++ Core.Numero/Numero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Numero
+{
+    public class Numero
+    {
+        public String elNumero { get; set; } //  una propiedad para el numero y una propiedad para la base 
+        public int laBase { get; set; }
+
+        public bool esBaseDiez { get {
+                return laBase == 10 ; }
+        }
+
+        public double elNumeroEnDecimal { get {
+                return double.Parse(elNumero); } }
+
+    }
+}
