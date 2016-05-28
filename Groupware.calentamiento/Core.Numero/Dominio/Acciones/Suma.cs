06d4700b75aa31d5c2964ae977b1888a6382ecd1
jeancapadilla <padilla-jean@hotmail.com>
2016-05-28 16:44:02 -0600

    semana 3 version suma

---
+++ Groupware.calentamiento/Core.Numero/Dominio/Acciones/Suma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Numero.Dominio.Acciones
+{
+    public class Suma
+    {
+        public Numero Operar (Numero elPrimerNumero, Numero elSegundoNumero)
+        {
+            Numero resultado = null;
+            var validarBases = new Validaciones.ValidarBase();
+            if(validarBases.LasDosBasesSonIguales(elPrimerNumero,elSegundoNumero))
+            { 
+                double elResultadoNumerico = (elPrimerNumero.elNumeroEnDecimal + elSegundoNumero.elNumeroEnDecimal);
+                // toDo: Convertir el resultado en Base diez del punto anterior a la base de los numero 
+                // por ejemplo: prodria estar sumando "2A" + "3F" en base hexadecimal 
+                resultado = new Numero(elResultadoNumerico.ToString(), elPrimerNumero.laBase);
+            }
+            return (resultado);
+        }
+    }
+}
--- Groupware.calentamiento/Core.Numero/Numero.cs
+++ Groupware.calentamiento/Core.Numero/Numero.cs
@@ -18,5 +18,27 @@ namespace Core.Numero
         public double elNumeroEnDecimal { get {
                 return double.Parse(elNumero); } }
 
+        public Numero(string elNumero, int laBase)
+        {
+            // instancie una validacion del numero
+            Numero elResultado;
+            var validacionBase = new Dominio.Validaciones.ValidarBase();
+            var validacionNumero = new Dominio.Validaciones.ValidarNumero();
+            if (validacionBase.LaBaseEstaEnElIntervaloCorrecto(laBase) &
+                validacionNumero.ElNumeroEsValidoEnLaBase(elNumero, laBase))
+            {
+                
+                this.laBase = laBase;
+                this.elNumero = elNumero;
+
+            }
+            else
+            {                
+            }                
+        }
+
+        public Numero()
+        {
+        }
     }
 }
